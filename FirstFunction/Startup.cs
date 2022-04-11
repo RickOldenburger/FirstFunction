b3906518a40a3ef8f91a92027cbcbc17e5179d80
Arianna Osar <ariannaosar@gmail.com>
2022-04-11 17:01:08 -0500

    Abstract serilog config to appsettings file

--- FirstFunction/Startup.cs
+++ FirstFunction/Startup.cs
@@ -16,29 +16,37 @@ namespace My.Function
 {
     public class Startup : FunctionsStartup
     {
-        const string SPLUNK_ENDPOINT = "https://splunk-hec.machinedata.illinois.edu:8088"; //  Your splunk url 
-        const string SPLUNK_HEC_TOKEN = "ca1a4a25-806a-4f5b-bf83-42b4732d2edb"; // Your HEC token.
-        const string SPLUNK_LOG_LEVEL = "WARNING";
-        const string CONSOLE_TEMPLATE = "";
-        const string SPLUNK_URI_PATH = "services/collector/event";
-        const string SPLUNK_SOURCE = "HttpTrigger1";
-        const string SPLUNK_SOURCE_TYPE = "TestDataBase";
-        const string SPLUNK_INDEX = "apps-sandbox-dev_techsvc-sdg";
-
         public override void Configure(IFunctionsHostBuilder builder)
         {
+          try {
+            // sleep
+            // System.Threading.Thread.Sleep(4000);
+            Console.WriteLine("[DEBUG] [Startup.Configure] Running startup.");
 
-            IConfigurationRoot configuration = new ConfigurationBuilder()
+            // Get the original configuration provider from the Azure Function
+            var configProvider = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+            IConfigurationRoot config = new ConfigurationBuilder()
                 .SetBasePath(Environment.CurrentDirectory)
+                .AddConfiguration(configProvider) // Add the original function configuration 
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .Build();
-            
+
+            builder.Services.AddSingleton(config);
             builder.Services.AddLogging(logBuilder => logBuilder
-                // .ClearProviders() // Generates error: Value cannot be null. (Parameter 'provider')
-                .AddSerilog(Serilogger.Create(SPLUNK_ENDPOINT, SPLUNK_HEC_TOKEN, 
-                    SPLUNK_LOG_LEVEL, CONSOLE_TEMPLATE, SPLUNK_URI_PATH, SPLUNK_SOURCE,
-                    SPLUNK_SOURCE_TYPE, SPLUNK_INDEX), true));
-     
+              .AddSerilog(Serilogger.Create(config), true)
+            );
+            // todo clear providers...?
+
+           } catch (Exception e) {
+            // Manually log error to console since logger code failed to initialize
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"[DEBUG] [Startup.Configure] Error: {e.Message}");
+            Console.Error.WriteLine(e.StackTrace);
+            Console.ResetColor();
+            // Propogate
+            throw e;
+          }
+    
         }
     }
 }
--- FirstFunction/StartupSerilog.cs
+++ FirstFunction/StartupSerilog.cs
@@ -2,30 +2,91 @@ using System;
 using Serilog;
 using Serilog.Events;
 using Serilog.Core;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace CustomLogger
 {
+    public class SplunkConfig
+    {
+        public string splunkHost { get; set; } // HostUrl
+        public string uriPath { get; set; } = "services/collector";
+        public string eventCollectorToken { get; set; }
+        public string sourceName { get; set; }
+        public string sourceType { get; set; }
+        public string index { get; set; }
+        public string host { get; set; } = Environment.MachineName;
+        public string logLevel { get; set; }
+
+        public SplunkConfig(IConfigurationRoot config) {
+            var fieldMappings = new Dictionary<string, string>{
+                {"Splunk:HostUrl", "splunkHost"},
+                {"Splunk:UriPath", "uriPath"},
+                {"Splunk:HecToken", "eventCollectorToken"},
+                {"Splunk:Source:Name", "sourceName"},
+                {"Splunk:Source:Type", "sourceType"},
+                {"Splunk:Index", "index"},
+                {"Splunk:Host", "host"},
+                {"Splunk:LogLevel", "logLevel"}
+            };
+
+            // TODO: Set explicit type
+            foreach(var item in config.GetSection("Splunk").AsEnumerable()) {
+                try {
+                    var key = item.Key;
+                    var value = item.Value;
+
+                    var fieldName = fieldMappings[key];
+                    // is this necessary
+                    if (fieldName.Length > 0 && value.Length > 0) {
+                        // Console.WriteLine($"[INFO] Setting field {fieldName} = [{value}]");
+                        this.GetType().GetProperty(fieldName).SetValue(this, value);
+                    }
+                } catch (Exception e) {
+                    // todo conditional on specific err code else throw
+                    // if (e.
+                    if (item.Value?.GetType() == typeof(string)) {
+                        Console.WriteLine($"[WARNING] [Serilogger.cs] Could not set value for {item.Key} even though a value is set. Skipping.");
+                        return;
+                    }
+                    // todo possibly ignore this. It logs for values of nested configs which is annoying and unnecessary.
+                    Console.WriteLine($"[INFO] [Serilogger.cs] Could not set value for {item.Key}. Skipping.");
+                }
+            }
+
+        }
+    }
     public static class Serilogger
     {
+        // Defaults
         const string _template = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] [{Level}] {RequestId}-{SourceContext}: {Message}{NewLine}{Exception}";
+        const string _minLogLevel = "INFORMATION";
 
-        public static Logger Create(string endpoint, string hecToken, string logLevel = "INFORMATION",
-            string template = _template, string uriPath = "services/collector", string source = "", 
-            string sourceType = "", string index = "")
+        public static Logger Create(IConfigurationRoot config)
         {
-            LoggingLevelSwitch _levelSwitch = new LoggingLevelSwitch(getLogLevelByName(logLevel));
-            if (template == "")
-                template = _template;
+            Console.WriteLine("[DEBUG] Creating new logger");
+
+            // Get config values
+            string minLogLevel = config.GetValue("Serilog:MinimumLevel:Default", _minLogLevel);
+            string template = config.GetValue("Serilog:Template", _template);
+            IEnumerable<KeyValuePair<string, string>> serilogOverride = config.GetSection("Serilog:MinimumLevel:Override").AsEnumerable();
+
+            // Initialize splunk config
+            SplunkConfig splunkConfig = new SplunkConfig(config);
+
+            LoggingLevelSwitch _levelSwitch = new LoggingLevelSwitch(getLogLevelByName(minLogLevel));
 
             return (new LoggerConfiguration()
                 .WriteTo.Console(outputTemplate: template)
-                .WriteTo.EventCollector(splunkHost: endpoint
-                    , eventCollectorToken: hecToken
-                    , uriPath: uriPath
-                    , source: source
-                    , sourceType: sourceType
-                    , host: Environment.MachineName
-                    , index: index)
+                .WriteTo.EventCollector(
+                    splunkHost: splunkConfig.splunkHost,
+                    eventCollectorToken: splunkConfig.eventCollectorToken,
+                    uriPath: splunkConfig.uriPath,
+                    source: splunkConfig.sourceName,
+                    sourceType: splunkConfig.sourceType,
+                    host: splunkConfig.host,
+                    index: splunkConfig.index
+                )
                 //.ReadFrom.Configuration(configuration) Broken in .net5 and .net6
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
